a90e91c0c008b4f89b8483c696fca4a5c400e8ea
github username <github email address>
2020-10-07 16:14:44 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,'Mr. Chocolate', 24, 'M', 'brown', 'sweet', 1);
+INSERT INTO bears VALUES (2, 'Rowdy', 100, 'F', 'black', 'angery', 1);
+INSERT INTO bears VALUES (3,'Tabitha', 88, 'F', 'black', 'love', 1);
+INSERT INTO bears VALUES (4,'Sergeant Brown', 10, 'M', 'brown', 'stupid', 1);
+INSERT INTO bears VALUES (5,'Melissa', 23, 'F', 'pink', 'angel', 1);
+INSERT INTO bears VALUES (6,'Grinch', 60, 'M', 'polar', 'angery', 1);
+INSERT INTO bears VALUES (7,'Wendy', 45, 'F', 'pink', 'happi', 1);
+INSERT INTO bears VALUES (9, null , 55, 'F', 'black', 'sad', 1);
